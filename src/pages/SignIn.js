632d233ae39092e4ac7641420474877acfcbe6e9
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2021-04-20 15:44:25 -0400

    login working

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -3,30 +3,25 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Alert from '@material-ui/lab/Alert';
+import axios from 'axios';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
+      {'Gustavo Paulo. '}
       {new Date().getFullYear()}
-      {'.'}
     </Typography>
   );
 }
 
-const useStyles = makeStyles((theme) => ({
+const styles = (theme) => ({
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
@@ -44,74 +39,127 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-}));
+});
 
-export default function SignIn() {
-  const classes = useStyles();
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      pass: '',
+      loading: false,
+      errorMsg: null,
+    };
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handlePass = this.handlePass.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+  handleEmail(ev) {
+    this.setState({
+      ...this.state,
+      email: ev.target.value,
+    });
+  }
+  handlePass(ev) {
+    this.setState({
+      ...this.state,
+      pass: ev.target.value,
+    });
+  }
+  handleLogin(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    this.setState({ ...this.state, loading: true });
+    axios.put(`https://dev.tuten.cl:443/TutenREST/rest/user/${encodeURI(this.state.email)}`, null, {
+      headers: {
+        'Password': this.state.pass,
+        'App': 'APP_BCK',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      this.setState({ ...this.state, loading: false });
+      console.log(res.data['sessionTokenBck']);
+    }).catch((err) => {
+      this.setState({
+        ...this.state,
+        loading: false, errorMsg: err.message,
+      })
+    })
+  }
+
+  render() {
+    const { classes } = this.props;
+    const loading = this.state.loading && (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress color="secondary" />
       </Box>
-    </Container>
-  );
+    );
+    const errMsg = (
+      this.state.errorMsg &&
+        <Alert severity="error">{this.state.errorMsg}</Alert>
+    );
+
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Iniciar Sesión
+          </Typography>
+          <form className={classes.form} noValidate>
+            {errMsg}
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Correo Electrónico"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleEmail}
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Contraseña"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={this.state.pass}
+              onChange={this.handlePass}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              disabled={this.props.loading}
+              className={classes.submit}
+              onClick={this.handleLogin}
+            >
+              Iniciar Sesión
+            </Button>
+            {loading}
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    );
+  }
 }
+
+export default withStyles(styles, { withTheme: true })(SignIn);
